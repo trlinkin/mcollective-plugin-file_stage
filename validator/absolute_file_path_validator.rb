91f96f15f9a6b8a1d02cddade48bda109e1ecd49
Thomas Linkin <trlinkin@gmail.com>
2014-04-23 14:11:18 -0400

    Correct variable usage in absolute_file_path validation
    The `absolute_file_path` validator was using missmatched variables
    halfway through the code.
    
    The commit realigns everything to use more ceoherent and clearer named
    variables.

--- validator/absolute_file_path_validator.rb
+++ validator/absolute_file_path_validator.rb
@@ -3,11 +3,11 @@ require 'pathname'
 module MCollective
   module Validator
     class Absolute_file_pathValidator
-      def self.validate(path)
-        Validator.typecheck(src, :string)
-        Validator.validate(src, :shellsafe)
+      def self.validate(raw_path)
+        Validator.typecheck(raw_path, :string)
+        Validator.validate(raw_path, :shellsafe)
 
-        path = Pathname.new(src)
+        path = Pathname.new(raw_path)
         raise "Cannot use relative paths" unless path.absolute?
         raise "Path must be a file" unless path.file?
       end
